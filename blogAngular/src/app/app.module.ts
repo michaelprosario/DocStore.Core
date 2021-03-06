0aa94890d8ee74e737bc74f12ef059ffadecb976
Michael Rosario <michael.p.rosario@gmail.com>
2021-03-06 12:36:27 -0500

    feat ( app ) Setup primeeng

--- blogAngular/src/app/app.module.ts
+++ blogAngular/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MenuItem} from 'primeng/api';
+import {PanelMenuModule} from 'primeng/panelmenu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainMenuComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    PanelMenuModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
