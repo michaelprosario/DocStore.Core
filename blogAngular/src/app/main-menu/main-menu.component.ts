00e0c6e16cec70c1a10351c17411b566ebfe410c
Michael Rosario <michael.p.rosario@gmail.com>
2021-03-06 13:17:45 -0500

    feat ( menu ) Add post and pages

--- blogAngular/src/app/main-menu/main-menu.component.ts
+++ blogAngular/src/app/main-menu/main-menu.component.ts
@@ -15,77 +15,29 @@ export class MainMenuComponent implements OnInit {
   ngOnInit() {
       this.items = [
           {
-              label: 'File',
+              label: 'Content',
+              expanded: true,
               icon: 'pi pi-pw pi-file',
               items: [{
-                      label: 'New', 
-                      icon: 'pi pi-fw pi-plus',
+                      label: 'Posts', 
+                      icon: 'pi',
+                      expanded: true,
                       items: [
-                          {label: 'User', icon: 'pi pi-fw pi-user-plus'},
-                          {label: 'Filter', icon: 'pi pi-fw pi-filter'}
-                      ]
-                  },
-                  {label: 'Open', icon: 'pi pi-fw pi-external-link'},
-                  {separator: true},
-                  {label: 'Quit', icon: 'pi pi-fw pi-times'}
-              ]
-          },
-          {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-              ]
-          },
-          {
-              label: 'Help',
-              icon: 'pi pi-fw pi-question',
-              items: [
-                  {
-                      label: 'Contents',
-                      icon: 'pi pi-pi pi-bars'
-                  },
-                  {
-                      label: 'Search', 
-                      icon: 'pi pi-pi pi-search', 
-                      items: [
-                          {
-                              label: 'Text', 
-                              items: [
-                                  {
-                                      label: 'Workspace'
-                                  }
-                              ]
-                          },
-                          {
-                              label: 'User',
-                              icon: 'pi pi-fw pi-file',
-                          }
-                  ]}
-              ]
-          },
-          {
-              label: 'Actions',
-              icon: 'pi pi-fw pi-cog',
-              items: [
-                  {
-                      label: 'Edit',
-                      icon: 'pi pi-fw pi-pencil',
-                      items: [
-                          {label: 'Save', icon: 'pi pi-fw pi-save'},
-                          {label: 'Update', icon: 'pi pi-fw pi-save'},
+                          {label: 'Add post', icon: 'pi pi-plus'},
+                          {label: 'Posts', icon: 'pi pi-list'}
                       ]
                   },
                   {
-                      label: 'Other',
-                      icon: 'pi pi-fw pi-tags',
-                      items: [
-                          {label: 'Delete', icon: 'pi pi-fw pi-minus'}
-                      ]
-                  }
+                    label: 'Pages', 
+                    icon: 'pi',
+                    items: [
+                        {label: 'Add page', icon: 'pi pi-plus'},
+                        {label: 'Page', icon: 'pi pi-list'}
+                    ]
+                },                  
               ]
-          }
+          },
+          
       ];
   }
 }
